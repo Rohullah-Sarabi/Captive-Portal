48a15f2f5edfb39b675cd5c510c923b73947bbcf
Rohullah Sarabi <rohullah.sarabi313@gmail.com>
2025-06-28 10:52:23 +0430

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+   return(
+    <div>
+      <h1 className="text-awcc-blue">AWCC Captive Portal</h1>
+      <button className="bg-awcc-orange">Connect</button>
+      <h3 className='text-lg'>Welcome Rohullah</h3>
+    </div>
+   )
+}
+
+export default App
+
+
+
